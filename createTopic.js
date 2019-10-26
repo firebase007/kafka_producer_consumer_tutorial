10fb1d9330cdaee9ebdfa77458f918b285186488
Alexander Nnakwue <annakwue@terragonltd.com>
2019-10-26 15:14:53 +0100

    update

--- createTopic.js
+++ createTopic.js
@@ -1,16 +1,18 @@
-var kafka = require('kafka-node');
-var client = new kafka.KafkaClient({kafkaHost: config.KafkaHost});
+const kafka = require('kafka-node');
 const config  = require('./config');
+
+const client = new kafka.KafkaClient({kafkaHost: config.KafkaHost});
+
 
 
 const topicToCreate = [{
   topic: config.KafkaTopic,
   partitions: 1,
-  replicationFactor: 2
+  replicationFactor: 1
 }
 ];
 
 client.createTopics(topicToCreate, (error, result) => {
   // result is an array of any errors if a given topic could not be created
-  console.log(result, 'result');
+  console.log(result, 'topic created successfully');
 });
