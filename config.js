4e1ceda494da41fd1246d3781a96225bdddb4ec1
Dilraj Singh <dsomel21@gmail.com>
2020-10-23 23:19:31 -0400

    Update KAFKA_TOPIC to KAFKA_TOPIC_NAME in config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   KafkaHost:process.env.KAFKA_HOST,
-  KafkaTopic: process.env.KAFKA_TOPIC
+  KafkaTopic: process.env.KAFKA_TOPIC_NAME
 };
 
 module.exports = config;
